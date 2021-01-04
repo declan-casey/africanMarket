d7670936b319940c46cae7044bd159a190c7da4a
Jacobstephens17 <64627086+Jacobstephens17@users.noreply.github.com>
2021-01-04 14:51:10 -0800

    Built some of the first page and added a little styling, still has a long way to go

--- africanmarket/src/App.js
+++ africanmarket/src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+import banner from './images/banner.jpg'
+
+
+/*Styling Using Styled Components*/
+const StyledHeadDiv = styled.div`
+background-image: url(banner);
+background-size:cover;
+height:100vh;
+
+`
+
+const StyledHeaderOne = styled.h1`
+color:white;
+background-color:black;
+text-align:center;
+`
+
+const StyledParagraph = styled.p`
+text-align: center;
+`
+
+const ActivateButton = styled.button`
+background-color:darkorange;
+color:white;
+border:none;
+border-radius:4px;
+display:flex;
+margin:0 auto;
+`
+const StyledHeaderTwo = styled.h2`
+color:white;
+background-color:black;
+text-align:center;
+`
+
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <StyledHeadDiv>
+        <nav>African Marketplace</nav>
+
+        <StyledHeaderOne>African Marketplace</StyledHeaderOne>
+
+        <StyledParagraph>SAUTI AFRICA EMPOWERS SMALL BUSINESS OWNERS, PARTICULARLY WOMEN,
+TO IMPROVE THEIR BUSINESS AND ECONOMIC OPPORTUNITIES
+TO GROW OUT OF POVERTY.</StyledParagraph>
+
+        <ActivateButton>ACTIVATE</ActivateButton>
+
+        {/* <button>LEARN MORE</button> */}
+      
+      </StyledHeadDiv>
+
+      <div>
+        <StyledHeaderTwo>ARCU ALIQUET VEL LOBORTIS ATA NISL
+EGET AUGUE AMET ALIQUET NISL CEP DONEC</StyledHeaderTwo>
+        <StyledParagraph>Aliquam ut ex ut augue consectetur interdum. Donec amet imperdiet eleifend
+fringilla tincidunt. Nullam dui leo Aenean mi ligula, rhoncus ullamcorper.</StyledParagraph>
+      </div>
+
     </div>
+
   );
+
 }
 
 export default App;
