409428b64ea3ea313594f8645a189f23d48ea307
Jacobstephens17 <64627086+Jacobstephens17@users.noreply.github.com>
2021-01-04 16:42:16 -0800

    added the rest of the page content except for icons, needs more work on styling still

--- africanmarket/src/App.js
+++ africanmarket/src/App.js
@@ -10,31 +10,37 @@ const StyledApp = styled.div`
 display: flex;
 flex-direction:column;
 `
-const StyledNav = styled.nav`
+const Nav = styled.nav`
 font-size:25px;
 font-weight:bold;
 `
 
-const StyledHeadImg = styled.div`
+const HeadImg = styled.div`
 height:10vh;
+
 `
 
-const StyledImg = styled.img `
-  background-image: url="./images/banner.jpg";
+const ImgTop = styled.img `
   height:50vh;
 
+
 `
-const StyledHeaderOne = styled.h1`
+const HeaderOne = styled.h1`
 color:black;
 text-align:center;
+border-top:solid 3px black;
+border-bottom:solid 3px black;
+width:50%;
+margin:0 auto;
 background-color:lightgrey;
 
 `
 
-const StyledParagraph = styled.p`
+const Paragraph = styled.p`
 text-align: center;
 color:white;
-background-color:lightgrey;
+width:65%;
+margin: 0 auto;
 
 `
 
@@ -43,59 +49,225 @@ background-color:red;
 color:white;
 border:none;
 border-radius:4px;
+font-size:15px;
+padding:1rem 1.5rem;
 display:flex;
 margin: 5rem auto 20rem auto;
+
 `
 
-const StyledCenterDiv = styled.div`
+
+const CenterDiv = styled.div`
 color:black;
-background-color:lightgrey;
+background-color:#00FFFF;
 
 `
 
-const StyledHeaderTwo = styled.h2`
+const HeaderTwo = styled.h2`
 color:white;
-background-color:lightgrey;
+width:65%;
+margin: 0 auto;
 text-align:center;
+border-bottom: solid 1.5px darkgrey;
 
 `
 
+const Gallery = styled.div`
+display:flex;
+flex-direction:column;
+
+`
+
+const GalleryOne = styled.div`
+display:flex;
+align-items:center;
+justify-content:center;
+flex-direction:column;
+width:100%;
+background-color:lightgrey;
+
+`
+
+const GalleryTwo = styled.div`
+display:flex;
+align-items:center;
+justify-content:center;
+flex-direction:column;
+width:100%;
+background-color:darkgrey;
+
+`
+
+const GalleryThree = styled.div`
+display:flex;
+align-items:center;
+justify-content:center;
+flex-direction:column;
+width:100%;
+background-color:darkslategray;
+`
+
+const BottomDiv = styled.div`
+display:flex;
+align-items:center;
+justify-content:center;
+flex-direction:column;
+`
+
+const BottomDivHeading = styled.h2`
+display:flex;
+align-items:center;
+justify-content:center;
+`
+
+const BottomDivPar = styled.p`
+display:flex;
+align-items:center;
+justify-content:center;
+`
+
+const BottomDivContent = styled.h2`
+display:flex;
+align-items:center;
+justify-content:center;
+
+`
+
+const FooterOptions = styled.div`
+display:flex;
+align-items:center;
+justify-content:center;
+padding: 3rem;
+background-color:black;
+`
 
 
+const FooterContent = styled.div`
+display:flex;
+flex-direction:column;
+width:25%;
+color:white;
+
+`
+const FooterButtons = styled.div`
+display:flex;
+flex-direction:column;
+`
+const FooterHead = styled.h2`
+
+`
+
+const FooterPar = styled.p`
+
+`
+
+const LearnButton = styled.button`
+background-color:black;
+color:white;
+border:none;
+border-radius:4px;
+font-size:15px;
+padding:1rem 1.5rem;
+display:flex;
+margin: 3rem auto 3rem auto;
+border: 2px solid white;
+
+`
+const ActivateButtonFoot = styled.button`
+background-color:red;
+color:white;
+border:none;
+border-radius:4px;
+font-size:15px;
+padding:1rem 1.5rem;
+display:flex;
+margin: 3rem auto 0rem auto;
+
+`
+const Social = styled.div`
+
+`
 
 function App() {
-
   return (
-
     <StyledApp >
 
-      <StyledNav>African Marketplace</StyledNav>
+      <Nav>African Marketplace</Nav>
 
-        <StyledHeadImg>
-          <StyledImg  src="https://www.pixelstalk.net/wp-content/uploads/2016/08/Colorful-Fresh-Fruit-Background.jpg" />
-        </StyledHeadImg>
+        <HeadImg>
+          {/* <ImgTop  src="https://www.pixelstalk.net/wp-content/uploads/2016/08/Colorful-Fresh-Fruit-Background.jpg" /> */}
+        </HeadImg>
 
-        <StyledHeaderOne>African Marketplace</StyledHeaderOne>
+        <HeaderOne>African Marketplace</HeaderOne>
 
-        <StyledParagraph>SAUTI AFRICA EMPOWERS SMALL BUSINESS OWNERS, PARTICULARLY WOMEN,
+        <Paragraph>SAUTI AFRICA EMPOWERS SMALL BUSINESS OWNERS, PARTICULARLY WOMEN,
 TO IMPROVE THEIR BUSINESS AND ECONOMIC OPPORTUNITIES
-TO GROW OUT OF POVERTY.</StyledParagraph>
+TO GROW OUT OF POVERTY.</Paragraph>
 
         <ActivateButton>ACTIVATE</ActivateButton>
 
+      <CenterDiv>
 
+        <HeaderTwo>ARCU ALIQUET VEL LOBORTIS ATA NISL
+EGET AUGUE AMET ALIQUET NISL CEP DONEC</HeaderTwo>
 
+        <Paragraph>Aliquam ut ex ut augue consectetur interdum. Donec amet imperdiet eleifend
+fringilla tincidunt. Nullam dui leo Aenean mi ligula, rhoncus ullamcorper.</Paragraph>
 
-      <StyledCenterDiv>
+      </CenterDiv>
 
-        <StyledHeaderTwo>ARCU ALIQUET VEL LOBORTIS ATA NISL
-EGET AUGUE AMET ALIQUET NISL CEP DONEC</StyledHeaderTwo>
+      <Gallery>
 
-        <StyledParagraph>Aliquam ut ex ut augue consectetur interdum. Donec amet imperdiet eleifend
-fringilla tincidunt. Nullam dui leo Aenean mi ligula, rhoncus ullamcorper.</StyledParagraph>
+        <GalleryOne>
+          <h2>MAGNA PRIMIS LOBORTIS
+SED ULLAMCORPER</h2>
+<p>Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.</p>
+        </GalleryOne>
 
-      </StyledCenterDiv>
+        <GalleryTwo>
+          <h2>TORTOR DOLORE FEUGIAT
+ELEMENTUM MAGNA</h2>
+<p>Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.</p>
+        </GalleryTwo>
       
+        <GalleryThree>
+        `<h2>AUGUE ELEIFEND ALIQUET
+SED CONDIMENTUM</h2>
+<p>Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.</p>
+        </GalleryThree>
+
+      </Gallery>
+
+      <BottomDiv>
+        <BottomDivHeading>ACCUMSAN MUS TORTOR NUNC ALIQUET</BottomDivHeading>
+
+        <BottomDivPar>ut ex ut augue consectetur interdum. Donec amet imperdiet eleifend
+fringilla tincidunt. Nullam dui leo Aenean mi ligula, rhoncus ullamcorper.</BottomDivPar>
+      
+      <BottomDivContent>
+
+      </BottomDivContent>
+
+      </BottomDiv>
+
+      <FooterOptions>
+        <FooterContent>
+        <FooterHead>ARCUE UT VEL COMMODO</FooterHead>
+        <FooterPar>Aliquam ut ex ut augue consectetur interdum endrerit imperdiet amet eleifend fringilla.</FooterPar>
+        </FooterContent>
+
+        <FooterButtons>
+      <ActivateButtonFoot>ACTIVATE</ActivateButtonFoot>
+
+      <LearnButton>Learn More</LearnButton>
+
+        </FooterButtons>
+
+      </FooterOptions>
+
+
+      <Social></Social>
+
     </StyledApp>
 
   );
