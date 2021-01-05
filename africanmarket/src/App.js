e2def32c3c565144c6a7b7efe6af15e03a242213
RemyVila <josephvila123@gmail.com>
2021-01-05 16:44:25 -0500

    Register form + schema, browser router set linking, header

--- africanmarket/src/App.js
+++ africanmarket/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
-import Login from './components/Login';
+// import Login from './components/Login';
+import Register from './components/Register';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <Header />
+      <div className="App">
+          <Register />
+      </div>
+    </Router>
   );
 }
 
