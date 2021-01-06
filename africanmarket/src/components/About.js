c9f52b3adefa00acc30e57e75306986fc3829979
Jacob Stephens <jacobstephens2@gmail.com>
2021-01-06 15:22:33 -0800

    fixed position of burger

--- africanmarket/src/components/About.js
+++ africanmarket/src/components/About.js
@@ -2,41 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import { slide as Hamburger } from "react-burger-menu";
 
-
-const NavTitle = styled.nav`
-font-size:30px;
-font-weight:bold;
-color:white;
-background-color:#2c333a;
-padding:2rem 0rem 2rem 1.5rem;
-text-align:left;
-`
-
-
-
-const Menu = styled.div`
-align-items:right;
-display:flex;
-margin:0 0 0 100%;
-`
-
-
-const Nav = styled.nav`
-color:white;
-display:flex;
-align-items:right;
-`
-
-const A = styled.a`
-color:white;
-background-color:#21B2A6;
-padding:2rem;
-font-size:1.6rem;
-text-align:left;
-text-decoration:none;
-border-bottom: solid 1.5px darkgrey;
-`
-
 function About (){
 
 
