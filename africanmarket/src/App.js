7668eabbd8a22c92168adfd910695c9382522f04
RemyVila <josephvila123@gmail.com>
2021-01-06 13:56:28 -0500

    Home, register routing complete

--- africanmarket/src/App.js
+++ africanmarket/src/App.js
@@ -9,9 +9,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Home />
         <Header />
-        <OwnerRegister />
       </div>
     </Router>
   );
--- africanmarket/src/components/Header.js
+++ africanmarket/src/components/Header.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import { Link, Switch, Route } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Link,
+    Route,
+    Switch,
+  } from 'react-router-dom';
 import Register from './OwnerRegister';
-// import Home from './Home';
+import Home from './Home';
 import Login from './Login';
 // import styled from 'styled-components';
 
@@ -10,12 +15,12 @@ const Header = () => {
         <nav>
             <h1>AFRICAN MARKET</h1>
             <Link to='/register'>Register</Link>
-            {/* <Link to='/'>Home</Link> */}
+            <Link to='/'>Home</Link>
             <Link to='/login'>Login</Link>
 
             <Switch>
                 <Route path="/register" component={Register} />
-                {/* <Route exact path="/" component={Home} /> */}
+                <Route exact path="/" component={Home} />
                 <Route path='/login' component={Login} />
             </Switch>
         </nav>
